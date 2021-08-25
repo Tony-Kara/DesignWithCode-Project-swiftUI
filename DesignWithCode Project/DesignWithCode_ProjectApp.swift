c168e715a303e7db3d524cf8920d4c79c190aba4
Tony-Kara <eniolaanthony@gmail.com>
2021-08-25 22:14:22 +0600

    Initial Commit

---
+++ DesignWithCode Project/DesignWithCode_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignWithCode_ProjectApp.swift
+//  DesignWithCode Project
+//
+//  Created by mac on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignWithCode_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
