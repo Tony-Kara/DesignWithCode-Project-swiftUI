2cfabb997bc19377de969364fd2c42115d2a062f
Tony-Kara <eniolaanthony@gmail.com>
2021-08-26 01:53:34 +0600

    Initial commit - elements on view

--- DesignWithCode Project/ContentView.swift
+++ DesignWithCode Project/ContentView.swift
@@ -9,13 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            
+            BackCardView() // use this to move the frame up, subview was extracted
+                .offset(x: 0, y: -20)
+            
+            
+            CardView()
+        }
+        
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                    
+                    
+                }
+                Spacer()
+                Image("Logo1")
+            }// padding for left and right
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            
+            
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top) // make adjustment to the image
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        
+        .frame(width: 300, height: 220)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+        
     }
 }
