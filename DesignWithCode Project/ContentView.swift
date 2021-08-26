73fda6351b3dc192901d9fef899abeabcea9e7ae
Tony-Kara <eniolaanthony@gmail.com>
2021-08-26 16:37:47 +0600

    Add color and rotation effect to views

--- DesignWithCode Project/ContentView.swift
+++ DesignWithCode Project/ContentView.swift
@@ -13,13 +13,27 @@ struct ContentView: View {
         ZStack {
             
             BackCardView() // use this to move the frame up, subview was extracted
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
                 .offset(x: 0, y: -40)
                 .scaleEffect(0.9) // third view C, view A and B is on top of this view, back view
+                .rotationEffect(.degrees(10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
             
             BackCardView() // second view B
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
                 .offset(x: 0, y: -20)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
             
             CardView() // first view A on top of view B, view in front
+                .blendMode(.hardLight) //this is a good effect for overlapping views
         }
         
         
@@ -74,10 +88,8 @@ struct BackCardView: View {
             Spacer()
         }
         
-        .frame(width: 300, height: 220)
-        .background(Color.blue)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+        .frame(width: 340, height: 220)
+        
         
     }
 }
